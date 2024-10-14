d1e5d4f9a0587d6a746fc760e01c0de032f7a923
Mona Lisa <anthonyrichardo080@gmail.com>
2024-10-14 16:58:49 +0100

    updating each the mortgage calculation and updating a parent div to each span element to indicate our error button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const clearButton = document.getElementById("clearbutton");
 const amount = document.getElementById("amount");
+const
 const term = document.getElementById("years");
 const rate = document.getElementById("interest");
 const radios = document.getElementsByName("mortgage");
+const mortgageType = document.getElementById("typeMortgage");
 const repayBtn = document.getElementById("calculateRepayment");
 const resultPanel = document.getElementById("result-panel");
 const result = document.getElementById("result");
@@ -10,21 +12,32 @@ const monthlyRepay = document.getElementById("repayMonthly");
 const termRepay = document.getElementById("termRepay");
 
 
-const unselected = amount.value == "" || term.value == "" || rate.value == "" || valueSelect() == null;
+const unselected = amount.value == "" || term.value == "" || rate.value == "";
+const clearfunction = () => {
+    let aamountValue = amount.value;
+    let termValue = term.value;
+    let rateValue = rate.value;
+    let selected = valueSelected();
 
+
+    amountVa
+}
 
 repayBtn.addEventListener("click", () => {
-    let selected = valueSelect();
-    if(selected == "repayment"){
-       resultPanel.classList.add("hidden");
-       result.classList.remove("hidden");
-       monthlyRepay.innerText = `€${formatNumberWithCommas(mortgage())}`;
-       termRepay.innerText = `€${formatNumberWithCommas(totalRepayment())}`;
-    } else if(unselected){
-        console.log('This is clean');
-    } else {
-        console.log("Hello world")
-    }
+   let selected = valueSelect();
+   if(selected == "repayment"){
+    resultPanel.classList.add("hidden");
+    result.classList.remove("hidden");
+    monthlyRepay.innerText = `€${formatNumberWithCommas(mortgage())}`;
+    termRepay.innerText =  `€${formatNumberWithCommas(totalRepayment())}`;
+   }else if(selected == "interestOnly"){
+    resultPanel.classList.add("hidden");
+    result.classList.remove("hidden");
+    monthlyRepay.innerText = `€${formatNumberWithCommas(interestTerm())}`;
+    termRepay.innerText = `€${formatNumberWithCommas(interestRepay())}`;
+   } else if(selected = "null" || unselected){
+    console.log("empty")
+   }
 })
 
 function valueSelect(){
@@ -42,7 +55,33 @@ function formatNumberWithCommas(number) {
     return number.toString().replace(/(?<=\d)(?=(\d{3})+(?!\d))/g, ",");
 }
 
-console.log(formatNumberWithCommas(1234567890)); // Output: 1,234,567,890
+ // Output: 1,234,567,890
+
+//Monthly payment for interest only = interest rate% * loan amount / 12;
+
+const interestTerm = () => {
+    let amountValue = amount.value;
+    let rateValue = rate.value;
+
+    let P = Number(amountValue);
+    let r = (Number(rateValue) / 100);
+
+    let total = (P * r) / 12;
+    return total.toFixed(2);
+}
+
+console.log(interestTerm());
+ 
+const interestRepay = () => {
+    let termValue = term.value;
+    
+    let n = Number(termValue) * 12;
+
+   let total = n * interestTerm();
+
+    return total.toFixed(2);
+}
+
 
 
 const mortgage = () => {
